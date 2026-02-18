c0a3e0f68b69df1f0cfd743c2a605a0ed15e068d
verse-37 <nguyentrongquang2008@gmail.com>
2026-02-18 16:05:20 +0700

    Open new Maths section
    Open the new Maths section + Updates to the boilerplate.

---
+++ math-journal/load-mathjax.js
@@ -0,0 +1,9 @@
+window.MathJax = {
+};
+
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@4/tex-mml-chtml.js';
+  script.defer = true;
+  document.head.appendChild(script);
+})();
